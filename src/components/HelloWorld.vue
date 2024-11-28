639dd81b8882a93ad1a70b9a98bb27c78d7b89bb
ana <ana.ehe.ifct65@gmail.com>
2024-11-28 16:50:58 +0300

    añadir elemento al helloworld.vue

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,52 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+      <label for="hola">Hola </label>
+      <input type="text" placeholder="Escribe tu saludo">
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+input{
+  padding: 0.5em;
+  border-radius: 5px;
+  background-color: aquamarine;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
